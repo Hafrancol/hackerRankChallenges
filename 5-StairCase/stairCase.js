d7f45265b5ad11ea09048eef90ecd5c70c3e6a82
Alejandro Franco <alejofranco02@gmail.com>
2021-08-25 16:11:47 -0500

    Add the the solution in Js 'CODE', pdf, testcases.

---
+++ 5-StairCase/stairCase.js
@@ -0,0 +1,18 @@
+  // Exericise to show  a stair, we use math for in every cycle add "#" and less " ",using string method is call .repeat();
+	//in every cycle the space is less one, so we are sure the space is decreasing 
+	// Write your code here
+	const stairOfSize=n+1;
+	let space=" "
+	let hashtag="#"
+	
+	let numberOfSpace=n;
+	
+	for(let i=1;i<stairOfSize;i++){
+			numberOfSpace-=1;
+			console.log(space.repeat(numberOfSpace) + hashtag.repeat(i))
+			
+	}
+	
+	
+
+
