371befb8bb366f69907113107e462677113d92f0
Alejandro Franco <alejofranco02@gmail.com>
2022-05-19 22:09:18 -0500

    Add the simpleArraySum in C#

---
+++ Algorithms/1-SimpleArraySum/simpleArraySum.cs
@@ -0,0 +1,9 @@
+
+    public static int simpleArraySum(List<int> ar)
+    {
+        int result = 0;
+        foreach(int x in ar){
+            result += x;
+        }
+        return result;
+    }
